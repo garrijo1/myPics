e3e0a9d5404fcaed1ffc574d5c286f148480a09e
Joseph Garrison <braisengeek@gmail.com>
2017-10-23 11:09:09 -0500

    initial commit

---
+++ server/app/controllers/myPics.js
@@ -0,0 +1,94 @@
+'use strict'
+
+var express = require('express'),
+    router = express.Router(),
+    logger = require('../../config/logger'),
+    mongoose = require('mongoose'),
+    User = mongoose.model('myPics');
+
+
+module.exports = function (app, config) {
+    app.use('/api', router);
+    
+    router.get('/myPics', function (req, res, next){
+        logger.log('Get all users', 'verbose');
+
+       var query = User.find()
+       .sort(req.query.order)
+       .exec()
+       .then(result => {
+      	if(result && result.length) {
+			res.status(200).json(result);
+		} else {
+			res.status(404).json({message: "No users"});
+		}
+       })
+       .catch(err => {
+         return next(err);
+       });
+   })
+
+    router.get('mypics/userId', function (req, res, next){
+        logger.log('Get user'+ req.params.userId, 'verbose');
+
+       User.findById(req.params.userId)
+                   .then(user => {
+                       if(user){
+                           res.status(200).json(user);
+                       } else {
+                           res.status(404).json({message: "No user found"});
+                       }
+                   })
+                   .catch(error => {
+                       return next(error);
+                   });
+           });    
+
+    router.post('mypics/userId', function(req, res, next){
+        logger.log('Create user', 'verbose');
+
+       var user = new User(req.body);
+        user.save()
+       .then(result => {
+           res.status(201).json(result);
+       })
+       .catch( err => {
+          return next(err);
+       });
+     })
+  
+    router.put('mypics/userId', function (req, res, next){
+        logger.log('Update user'+ req.params.userId, 'verbose');
+
+           User.findOneAndUpdate({_id: req.params.userId}, 		
+           req.body, {new:true, multi:false})
+               .then(user => {
+                   res.status(200).json(user);
+               })
+               .catch(error => {
+                   return next(error);
+               });
+       });  
+
+    router.delete('mypics/userId', function (req, res, next){
+        logger.log('Delete user'+ req.params.userId, 'verbose');
+
+       User.remove({ _id: req.params.userId })
+               .then(user => {
+                   res.status(200).json({msg: "User Deleted"});
+               })
+               .catch(error => {
+                   return next(error);
+               });
+       });
+
+    router.post('/login', function(req, res, next){
+        console.log(req.body);
+        var email = req.body.email
+        var password = req.body.password;
+  
+        var obj = {'email' : email, 'password' : password};
+      res.status(201).json(obj);
+  });
+  
+};
